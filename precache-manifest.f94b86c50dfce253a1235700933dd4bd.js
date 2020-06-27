1e51e902d3fd9b48b7f5c9081a2de209e9dd2ad5
Rodrigo Fernandes <rodrigorcfernandes@gmail.com>
2020-06-27 11:14:45 -0300

    Updates

---
+++ precache-manifest.f94b86c50dfce253a1235700933dd4bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd7e51f1721bae879f8baa96b0ee95b5",
+    "url": "/procura-imagens/index.html"
+  },
+  {
+    "revision": "35f2654de59532a1f517",
+    "url": "/procura-imagens/static/css/main.a45e65a8.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/procura-imagens/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/procura-imagens/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35f2654de59532a1f517",
+    "url": "/procura-imagens/static/js/main.a5d1aad9.chunk.js"
+  },
+  {
+    "revision": "b68e36513fac1161afd3",
+    "url": "/procura-imagens/static/js/runtime-main.4e9fc5bf.js"
+  }
+]);
